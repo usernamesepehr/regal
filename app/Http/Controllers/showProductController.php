659e899ff0bd9c12fe44f87008a176f94c2c08fd
sepehr <seeetayeshgholami85@gmail.com>
2025-08-22 01:25:22 -0700

    get product added

--- app/Http/Controllers/showProductController.php
+++ app/Http/Controllers/showProductController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Option;
 use App\Models\Product;
+use App\Models\Product_category;
+use App\Models\Product_option;
+use App\Models\Rate;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -18,6 +23,11 @@ class showProductController extends Controller
     public function get($slug)
     {
         $product = $this->product($slug);
+        $comments = $this->comments($product->id);
+        $rate = $this->rate($product->id);
+        $categories = $this->categories($product->id);
+        $options = $this->options($product->id);
+        return Inertia::render('product', ['product' => $product, 'comments' => $comments, 'rate' => $rate, 'categories' => $categories, 'options' => $options]);
     }
     private function products($per_page)
     {
@@ -27,12 +37,35 @@ class showProductController extends Controller
     private function mapProducts($products)
     {
         return $products->map(function($product) {
-            $product->images = $product->images()->get();
+            $product->image = $product->images()->first();
             return $product;
         });
     }
     private function product($slug)
     {
-        return Product::where('slug', $slug)->with('images')->first();
+        return Product::where('slug', $slug)->with('images')->firstOrFail();
+    }
+    private function categories($product_id)
+    {
+        return Product_category::where('product_id', $product_id)->with('category')->get();
+    }
+    private function comments($product_id)
+    {
+       return Comment::where('product_id', $product_id)->get();
+    }
+    private function rate($product_id)
+    {
+        return Rate::where('product_id', $product_id)->avg('rate');
+    }
+    private function options($product_id)
+    {
+        $product_options = Product_option::where('product_id', $product_id)->get();
+        $product_options->transform(function ($product_option) {
+            $options_id = array_map('intval', array_map('trim', explode(',', $product_option->options_id)));
+            $product_option->option1 = Option::where('id', $options_id[0])->first();
+            $product_option->option2 = Option::where('id', $options_id[1])->first();
+            return $product_option;
+        });
+        return $product_options;
     }
 }
